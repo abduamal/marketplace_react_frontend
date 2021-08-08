9ed69cd1b0e625677e3208febf439bd74e6c60f6
Amal Abdu <abduamal@ymail.com>
2021-08-08 12:38:23 -0500

    UI - render a list of shop name and industry for the fetchShops action

--- src/components/Shops.js
+++ src/components/Shops.js
@@ -4,14 +4,14 @@ import React from 'react'
 // to render the list of shops, this Shops function has access to the list of shops via ShopsContainer
 // the list is being called through the props argument
 function Shops(props) {
+  console.log(props)
   return(
     <div>
-      {props.shops.data.map((shop, index) =>
-        <li key={`${index}`}>
-          {shop.attributes.name} - {shop.attributes.industry}
-        </li>
+      {props.shops.map(shop =>
+        <li key={shop.id}>{shop.name} - {shop.industry}</li>
       )}
     </div>
-      )
+  )
 }
+
 export default Shops
