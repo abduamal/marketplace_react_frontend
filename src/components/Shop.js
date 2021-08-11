62d334f461c339ae4ca1cc2db7819960ed7feb10
Amal Abdu <abduamal@ymail.com>
2021-08-11 13:51:21 -0400

    Redirect can cause rerender issues; commented out b/c it is not needed.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import { Redirect } from 'react-router-dom'
 
 function Shop(props) {
-  console.log(props);
+
+  // props.shops is looking at all the shops via routerProps in ShopsContainer
+  // then, it is using the id to find the specific shop
   let shop = props.shops[props.match.params.id - 1]
   console.log(shop);
   return (
     <li>
+      {/* {shop ? null : <Redirect to='/shops'/>} */}
       {shop ? shop.name : null} - {shop? shop.industry : null}
     </li>
   )
