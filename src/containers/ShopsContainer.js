012bab119c8dffd22d48e1249546419b6ab8cb9f
Amal Abdu <abduamal@ymail.com>
2021-08-11 13:50:05 -0400

    wrapped routes in Switch to guarantee each page renders better

--- src/containers/ShopsContainer.js
+++ src/containers/ShopsContainer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+// Switch returns the first route that matches the set path when it is wrapped around Route paths
+//  - order matters: any dynamic route placed above a specific route will override the specific route and it won't render
+import { Route, Switch } from 'react-router-dom'
 import { fetchShops } from '../actions/fetchShops'
 import ShopInput from '../components/ShopInput'
 import Shops from '../components/Shops'
@@ -19,9 +21,12 @@ class ShopsContainer extends React.Component {
   render(){
     return(
       <div>
-        <Route path='/shops/new' component={ShopInput}/>
-        <Route path='/shops/:id'render={(routerProps) => <Shop {...routerProps} shops={this.props.shops}/>}/>
-        <Route exact path='/shops' render={(routerProps) => <Shops {...routerProps} shops={this.props.shops}/>}/>
+        <Switch>
+          <Route path='/shops/new' component={ShopInput}/>
+          <Route path='/shops/:id'render={(routerProps) => <Shop {...routerProps} shops={this.props.shops}/>}/>
+          <Route path='/shops' render={(routerProps) => <Shops {...routerProps} shops={this.props.shops}/>}/>
+        </Switch>
+
       </div>
     )
   }
