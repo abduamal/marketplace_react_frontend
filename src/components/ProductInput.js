cdb64b7d5a70fd3caff61bd13bf3ece11f44b3e8
Amal Abdu <abduamal@ymail.com>
2021-08-11 14:39:54 -0400

    add Product form and a list of a Shop's products...set up later

---
+++ src/components/ProductInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class ProductInput extends React.Component{
+  // constructor() {
+  //
+  // }
+
+  render() {
+    return(
+      <div>
+        ProductInput
+      </div>
+    )
+  }
+}
+export default ProductInput
---
+++ src/components/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Products(props) {
+  return(
+    <li>
+      Products
+    </li>
+  )
+}
+
+export default Products
