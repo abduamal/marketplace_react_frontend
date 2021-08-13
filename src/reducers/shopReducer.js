5abdaf08b5829a3362a4e6a84a2aa078b3fc2cad
Amal Abdu <abduamal@ymail.com>
2021-08-13 16:01:11 -0500

    include updated products to associated shop in reducer

--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -1,6 +1,6 @@
 // takes the previous state as the first argument, an action object as the second
 // then inside, the reducer is updated based on the action type
-// shop reducer is a fuction in essence
+// shop reducer is a function in essence
 
 export default function shopReducer(state = {shops: []}, action ) {
   switch (action.type) {
@@ -8,9 +8,16 @@ export default function shopReducer(state = {shops: []}, action ) {
       return {shops: action.payload}
     case 'ADD_SHOP':
       return {...state, shops: [...state.shops, action.payload]}
+    case 'ADD_PRODUCT':
+      let shops = state.shops.map(shop => {
+        if (shop.id === action.payload.id) {
+          return action.payload
+        } else {
+          return shop
+        }
+      })
+      return {...state, shops: shops}
     default:
-
+      return state
   }
-  return state
-
 }
