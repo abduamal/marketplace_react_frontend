84d9d375bb48b82418b9731116627a1425dedb62
Amal Abdu <abduamal@ymail.com>
2021-08-14 16:37:39 -0500

    add styling to Shops buttons

--- src/components/Shops.js
+++ src/components/Shops.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, Link} from 'react-router-dom'
 import Shop from './Shop'
+import Button from 'react-bootstrap/Button'
 
 
 // to render the list of shops, this Shops function has access to the list of shops via ShopsContainer
@@ -8,11 +9,18 @@ import Shop from './Shop'
 function Shops(props) {
   console.log(props)
   return(
-    <div>
+    // <div>
+    //   {props.shops.map(shop =>
+    //     <div key={shop.id}>
+    //       <Link to={`/shops/${shop.id}`}>{shop.name}</Link>
+    //     </div>
+    //   )}
+    // </div>
+    <div className="d-grid gap-2">
       {props.shops.map(shop =>
-        <div key={shop.id}>
-          <Link to={`/shops/${shop.id}`}>{shop.name}</Link>
-        </div>
+        <Button href={`/shops/${shop.id}`} key={shop.id} variant="outline-primary" size="lg">
+          {shop.name}
+        </Button>
       )}
     </div>
   )
