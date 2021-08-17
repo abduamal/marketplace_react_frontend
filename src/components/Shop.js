c34398546589fec068b151af320a731852943d52
Amal Abdu <abduamal@ymail.com>
2021-08-17 13:00:06 -0500

    add navigation button to shop show page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import ProductsContainer from '../containers/ProductsContainer'
 import '../index.css'
+import Button from 'react-bootstrap/Button'
 
 function Shop(props) {
 
   // props.shops is looking at all the shops via routerProps in ShopsContainer
   // then, it is using the id to find the specific shop
 
-  let shop = props.shops[props.match.params.id - 1]
-  console.log(shop);
+  const shop = props.shops[props.match.params.id - 1]
+  console.log(props)
+
   return (
     <div>
       <br/>
       <h4 className='pageTitle'>
         {shop ? shop.name : null} - {shop? shop.industry : null}
-      </h4><br/>
+      </h4>
+      <br/>
+      <Button href='/shops' variant="primary">Go Back</Button>
+      <br/>
+      <br/>
       <ProductsContainer shop={shop}/>
     </div>
 
